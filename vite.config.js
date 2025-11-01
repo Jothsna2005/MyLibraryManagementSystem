52b16f166b40721d240ff59198785c8b987fbc89
Jothsna <alumolujothsna281@gmail.com>
2025-11-01 16:40:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     rollupOptions: {
-      input: './index.html',
+      input: 'index.html',
     },
   },
 })
